45b88602d417f30b2bf25a13bf10e5be0f583876
Susanel92 <zuzannanizka@gmail.com>
2020-07-19 11:36:28 +0200

    Updates

---
+++ precache-manifest.a6d279158f8637df9b1e519f67c7f211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee19f3fd6119d0ca0e3c",
+    "url": "/shopping-list-app/static/css/main.957a3857.chunk.css"
+  },
+  {
+    "revision": "ee19f3fd6119d0ca0e3c",
+    "url": "/shopping-list-app/static/js/main.d47965d9.chunk.js"
+  },
+  {
+    "revision": "3ca11ef6bde1b037f7b4",
+    "url": "/shopping-list-app/static/js/runtime~main.a8b1d7f0.js"
+  },
+  {
+    "revision": "3b7da2fe5b8cd9df1604",
+    "url": "/shopping-list-app/static/js/2.8128d63f.chunk.js"
+  },
+  {
+    "revision": "2a9602695129634fed605f0e2d41e947",
+    "url": "/shopping-list-app/index.html"
+  }
+];
